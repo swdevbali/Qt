ae4ff1a5ee70a35dc957e55f470fcb9fde1c02de
swdev <swdev.bali@gmail.com>
2013-09-06 16:28:35 +0700

    notion of main application window

--- TextEditor/QtTextEditor.cc
+++ TextEditor/QtTextEditor.cc
@@ -2,20 +2,49 @@
 #include <QTextEdit>
 #include <QtGui>
 
-int main(int argv, char **args)
+
+class Notepad : public QWidget
 {
-    QApplication app(argv, args);
-    QTextEdit *textEdit = new QTextEdit;
-    QPushButton *btnOk = new QPushButton("OK");
-    QObject::connect(btnOk, SIGNAL(clicked()), qApp, SLOT(quit()));
+   
+public:
+    Notepad();
+         
+private slots:
+    void quit();
+
+private:
+    QTextEdit *textEdit;
+    QPushButton *btnOk;
+    
+};
+
+Notepad::Notepad()
+{
+    textEdit = new QTextEdit;
+    btnOk = new QPushButton("Quit");
+
+    connect(btnOk, SIGNAL(clicked()), qApp, SLOT(quit()));
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(textEdit);
     layout->addWidget(btnOk);
     
-    QWidget window;
-    window.setLayout(layout);
-    window.show();
+    setLayout(layout);
+    setWindowTitle("Better Notepad");
+
+};
+
+void Notepad::quit()
+{
+    qApp->quit();
+}
+
+int main(int argv, char **args)
+{
+    QApplication app(argv, args);
+    Notepad note;
+    note.resize(640,480);
+    note.show();
     
     return app.exec();
 }
